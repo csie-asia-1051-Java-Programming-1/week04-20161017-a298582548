cf4aca4502d2c1ea62250032a48fca6474857ebf
a298582548 <105021010@live.asia.edu.tw>
2016-10-17 16:08:41 +0800

    gg

---
+++ src/ex/ex01_105021010.java
@@ -0,0 +1,33 @@
+﻿package ex;
+/*
+ * Topic: 寫一個程式讓使多個正整數(包含0)直到使用者輸入-1 時停止，同時要顯示這些數字 總合 及 平均。
+ * Date: 2016/10/17
+ * Author: 105021010邱信喆
+ */
+import java.util.Scanner;
+public class ex01_105021010 {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("輸入一個值");
+		Scanner scn = new Scanner(System.in);
+		int b = 1;
+		int c = 0;
+		
+		while(true){
+		int a = scn.nextInt();
+		b = a+b;
+		c++;
+		if(a==-1){
+		System.out.println(b);
+		System.out.println((float)b/c);
+		break;
+		}
+		
+
+	}
+	}
+}
+
+
--- src/ex/ex02_105021010.java
+++ src/ex/ex02_105021010.java
@@ -2,10 +2,10 @@
 /*
  * Topic: 寫一個程式讓使用者輸入兩個正整數 n 與 m，然後顯示n x m 的乘法表，顯示完後問使用者是否繼續(Y/n) ，Y 可以重新輸入 n 則結束程式。
  * Date: 2016/10/17
- * Author: 1050210XX 周永振老師
+ * Author: 105021010 邱信喆
  */
-
-public class ex02_1050210XX {
+import java.util.
+public class ex02_105021010 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
